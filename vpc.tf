b259060345f6e1dc6c2865a6d97a82cf3787ac7c
Maisha Ahmed <58888586+maisha-ahmed@users.noreply.github.com>
2021-12-04 13:42:37 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,43 @@ resource "aws_vpc" "main" {
     Name = "Main VPC"
   }
 }
+
+resource "aws_subnet" "public01" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Public01"
+  }
+}
+
+resource "aws_subnet" "public02" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Public02"
+  }
+}
+
+resource "aws_internet_gateway" "ig1" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# 3. Create Custom Route Table
+resource "aws_route_table" "project1-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.ig1.id
+  }
+
+  tags = {
+    "Name" = "project1-route-table"
+  }
+}
